b2fdc5fa72aefddfcf36b0be520ce74fdc212e34
Joel Arvidsson <joel@oblador.se>
2019-12-03 00:12:17 +0100

    Fix line wrapping of grid axis label

--- src/ui/Grid.js
+++ src/ui/Grid.js
@@ -2,25 +2,31 @@ import React from 'react';
 import { FlexRow, styled, Text } from 'flipper';
 import { COLOR_SEPARATOR, COLOR_DISABLED } from './constants';
 
-const LABEL_HEIGHT = 15;
+const LABEL_HEIGHT = 12;
 const LABEL_MARGIN = 10;
 
 const SpacedRow = styled(FlexRow)({
   justifyContent: 'space-between',
 });
 
-const AxisText = styled(Text)({
-  fontSize: 12,
-  color: '#999',
+const AxisLabel = styled('div')({
+  position: 'relative',
+  width: 1,
+  marginBottom: LABEL_HEIGHT + LABEL_MARGIN,
+  backgroundColor: COLOR_SEPARATOR,
 });
 
-const Axis = ({ interval, numberOfIntervals, unit }) => (
-  <SpacedRow>
-    {new Array(numberOfIntervals).fill(null).map((_, i) => (
-      <AxisText>{i * interval}</AxisText>
-    ))}
-  </SpacedRow>
-);
+const AxisText = styled(Text)({
+  fontSize: 12,
+  lineHeight: 1,
+  color: '#999',
+  position: 'absolute',
+  top: '100%',
+  right: 0,
+  marginTop: LABEL_MARGIN,
+  textAlign: 'right',
+  whiteSpace: 'nowrap',
+});
 
 const identity = a => a;
 
@@ -50,26 +56,9 @@ export const Grid = ({
         value === 0 ? (
           <div />
         ) : (
-          <div
-            style={{
-              position: 'relative',
-              width: 1,
-              marginBottom: LABEL_HEIGHT + LABEL_MARGIN,
-              backgroundColor: COLOR_SEPARATOR,
-            }}
-          >
-            <AxisText
-              style={{
-                position: 'absolute',
-                top: '100%',
-                right: 0,
-                marginTop: LABEL_MARGIN,
-                textAlign: 'right',
-              }}
-            >
-              {formatLabel(value)}
-            </AxisText>
-          </div>
+          <AxisLabel>
+            <AxisText>{formatLabel(value)}</AxisText>
+          </AxisLabel>
         )
       )}
   </SpacedRow>
