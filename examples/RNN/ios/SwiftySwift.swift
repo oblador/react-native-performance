2e51e4383ccbba3db3964e74eb333c23c131a813
Joel Arvidsson <joel.arvidsson@klarna.com>
2020-11-25 15:51:40 +0100

    Update examples to use RN 0.63 and autolinking

--- examples/RNN/ios/SwiftySwift.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SwiftySwift.swift
-//  RNN
-//
-//  Created by Joel Arvidsson on 2019-12-01.
-//  Copyright © 2019 Facebook. All rights reserved.
-//
-
-import Foundation
--- examples/Vanilla/ios/SwiftySwift.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SwiftySwift.swift
-//  Example
-//
-//  Created by Joel Arvidsson on 2019-11-30.
-//  Copyright © 2019 Facebook. All rights reserved.
-//
-
-import Foundation
