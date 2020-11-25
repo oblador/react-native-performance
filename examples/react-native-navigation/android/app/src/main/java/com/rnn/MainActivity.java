addefbd615c8582243a3c54921d3ec77a3ac2658
Joel Arvidsson <joel@oblador.se>
2020-11-25 21:34:09 +0100

    Update examples to compile on android (#12)

--- examples/react-native-navigation/android/app/src/main/java/com/rnn/MainActivity.java
+++ examples/react-native-navigation/android/app/src/main/java/com/rnn/MainActivity.java
@@ -1,15 +1,6 @@
 package com.rnn;
 
-import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.NavigationActivity;
 
-public class MainActivity extends ReactActivity {
-
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "RNN";
-  }
+public class MainActivity extends NavigationActivity {
 }
--- examples/react-native-navigation/android/app/src/main/java/com/rnn/MainApplication.java
+++ examples/react-native-navigation/android/app/src/main/java/com/rnn/MainApplication.java
@@ -10,11 +10,13 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.reactnativenavigation.NavigationApplication;
+import com.reactnativenavigation.react.NavigationReactNativeHost;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends NavigationApplication {
 
   private final ReactNativeHost mReactNativeHost =
-      new ReactNativeHost(this) {
+      new NavigationReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
@@ -43,7 +45,6 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 
