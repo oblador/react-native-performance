2911c58ac59029a302256fab51049b965d06f386
Joel Arvidsson <joel@oblador.se>
2021-02-13 18:25:26 +0100

    Respect root project gradle settings

--- packages/react-native-performance/android/src/main/java/com/oblador/performance/PerformanceModule.java
+++ packages/react-native-performance/android/src/main/java/com/oblador/performance/PerformanceModule.java
@@ -18,7 +18,6 @@ import java.lang.StringBuffer;
 import java.util.Map;
 import java.util.HashMap;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
