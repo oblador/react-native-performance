6905d4420335b275a0e2a2d610298229165737e4
Olivier Payen <olivier.payen@gmail.com>
2022-06-30 11:15:43 +0200

    Call RN's performance.now()

--- packages/react-native-performance/android/src/main/cpp/RnPerformance.cpp
+++ packages/react-native-performance/android/src/main/cpp/RnPerformance.cpp
@@ -1,8 +1,9 @@
 #include <jni.h>
+#include <react/jni/NativeTime.h>
 
 extern "C" JNIEXPORT jlong JNICALL
 Java_com_oblador_performance_PerformanceModule_rnPerformanceNow(
         JNIEnv* env,
         jobject /* this */) {
-    return 42;
+    return facebook::react::reactAndroidNativePerformanceNowHook();
 }
