2712116d159db0cb9bff84dc812c10f0c416e4e2
Olivier Payen <olivier.payen@klarna.com>
2022-09-16 17:39:38 +0200

    Use RN's code to get the timestamp

--- packages/react-native-performance/ios/RNPerformance.h
+++ packages/react-native-performance/ios/RNPerformance.h
@@ -1,8 +1,17 @@
 #import "RNPerformanceEntry.h"
+#include <chrono>
 
 static int64_t RNPerformanceGetTimestamp()
 {
-    return CACurrentMediaTime() * 1000;
+    // Copied from https://github.com/facebook/react-native/blob/main/React/CxxBridge/RCTJSIExecutorRuntimeInstaller.mm#L25
+    auto time = std::chrono::steady_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(
+                        time.time_since_epoch())
+                        .count();
+
+    constexpr double NANOSECONDS_IN_MILLISECOND = 1000000.0;
+
+    return duration / NANOSECONDS_IN_MILLISECOND;
 }
 
 NSString * _Nonnull const RNPerformanceEntryWasAddedNotification = @"RNPerformanceEntryWasAdded";
