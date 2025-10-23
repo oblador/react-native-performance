61af8528c82b285b5cd401c7b611e3711c4b9482
Joel Arvidsson <joel@oblador.se>
2025-10-23 17:16:18 +0200

    chore: drop flow in native spec (#120)

--- packages/react-native-performance/src/NativeRNPerformanceManager.ts
+++ packages/react-native-performance/src/NativeRNPerformanceManager.ts
@@ -1,14 +1,10 @@
-/**
- * @flow
- */
-
 import type { TurboModule } from 'react-native/Libraries/TurboModule/RCTExport';
 import { TurboModuleRegistry } from 'react-native';
 
 export interface Spec extends TurboModule {
   // Events
-  +addListener: (eventName: string) => void;
-  +removeListeners: (count: number) => void;
+  addListener: (eventName: string) => void;
+  removeListeners: (count: number) => void;
 }
 
-export default (TurboModuleRegistry.get<Spec>('RNPerformanceManager'): ?Spec);
+export default TurboModuleRegistry.get<Spec>('RNPerformanceManager');
